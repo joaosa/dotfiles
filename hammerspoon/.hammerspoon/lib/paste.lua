344aa6131d08db57b506db563397911f27e908aa
João Andrade <sousa.andrade.joao@gmail.com>
2026-02-10 13:41:23 +0000

    chore(hammerspoon): remove dead clipboard code from paste

--- hammerspoon/.hammerspoon/lib/paste.lua
+++ hammerspoon/.hammerspoon/lib/paste.lua
@@ -2,20 +2,12 @@
 -- Paste Utilities
 -----------------------------------------------
 
-local clipboard = require("lib.clipboard")
-local withClipboard = clipboard.withClipboard
-
--- Paste string by temporarily replacing clipboard
-local function pasteString(string)
-    if not string or string == "" then
+local function pasteString(str)
+    if not str or str == "" then
         return false
     end
 
-    withClipboard(function()
-        hs.pasteboard.setContents(string)
-        hs.eventtap.keyStrokes(string)
-    end)
-
+    hs.eventtap.keyStrokes(str)
     return true
 end
 
