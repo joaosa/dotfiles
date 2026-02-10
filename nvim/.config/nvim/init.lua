e2cb15fe851367f517c8763a3fed29cf3af05d9d
João Andrade <sousa.andrade.joao@gmail.com>
2026-02-10 14:22:27 +0000

    chore(nvim): async treesitter parser install

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -563,7 +563,7 @@ require 'nvim-treesitter.configs'.setup {
     "markdown_inline",
   },
 
-  sync_install = true,
+  sync_install = false,
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
