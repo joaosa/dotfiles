a285fa586ada207cb21876600dd9643b9b8167ae
João Andrade <sousa.andrade.joao@gmail.com>
2026-02-18 11:05:55 +0000

    feat(nvim): enable blink.cmp auto-show documentation

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -75,6 +75,12 @@ require("lazy").setup({
     opts = {
       keymap = { preset = "default" },
       appearance = { nerd_font_variant = "mono" },
+      completion = {
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 200,
+        },
+      },
       sources = {
         default = { "lsp", "path", "snippets", "buffer" },
         per_filetype = {
